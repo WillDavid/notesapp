4adf9d110dbdc1d4b89a678c400449b4702e3dd3
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-09-01 21:24:36 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { registerServiceWorker } from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
     <App />,document.getElementById('root')
 );
 
-registerServiceWorker();
+serviceWorkerRegistration.register();
